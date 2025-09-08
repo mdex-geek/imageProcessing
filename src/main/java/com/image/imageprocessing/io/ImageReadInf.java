cdbec969b6d19b3950644a263daadf134fa22ffb
deepanshu garg <greekof022@gmail.com>
2025-09-08 13:03:09 +0530

    init

---
+++ src/main/java/com/image/imageprocessing/io/ImageReadInf.java
@@ -0,0 +1,9 @@
+package com.image.imageprocessing.io;
+
+import java.awt.image.BufferedImage;
+
+public interface ImageReadInf {
+     <T> BufferedImage readImage(T src);
+
+     void sendImage(BufferedImage image);
+}
